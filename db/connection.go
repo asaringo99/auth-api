e64750b77b6c2a0bddb64d0752aac26ecc03b8ee
asaringo99 <ka99code@gmail.com>
2023-05-21 17:10:32 +0900

    db connection

---
+++ db/connection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewAuthDbConnection() *gorm.DB {
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	user := os.Getenv("MYSQL_ROOT_USER")
+	password := os.Getenv("MYSQL_ROOT_PASSWORD")
+	database := os.Getenv("MASQL_DATABASE")
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+	if err != nil {
+		fmt.Println(connection)
+		panic(err)
+	}
+	return db
+}
